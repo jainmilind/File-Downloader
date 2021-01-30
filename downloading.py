75cedc2e83b5f83dfdf1d9a4fb5db3ede558e066
Milind Jain <jainmilind10112002@gmail.com>
2021-01-30 20:17:48 +0530

    Added progress bar

--- downloading.py
+++ downloading.py
@@ -1,11 +1,22 @@
 import requests
-def download(url,place):
-    r = requests.get(url, allow_redirects=True)
-    n = len(url)
-    k = '.png'
-    for i in range(n-1,-1,-1):
-        if url[i] == '.':
-            k = url[i:]
-            break
-    with open (place + k, 'wb') as file:
-        file.write(r.content)
+def download(url , place):
+    with open(place , 'wb') as f:
+        response = requests.get(url, allow_redirects=True,stream=True)
+        total_length = response.headers.get("content-length")
+        if total_length is None:
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(100 * dl / total_length)
+                #self.progressbar1.setvalue(done)
+                print(done)
+    # n = len(url)
+    # k = '.png'
+    # for i in range(n-1,-1,-1):
+    #     if url[i] == '.':
+    #         k = url[i:]
+    #         break
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
-
+import requests
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(893, 603)
+        MainWindow.resize(893, 560)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.Heading = QtWidgets.QLabel(self.centralwidget)
@@ -27,6 +27,7 @@ class Ui_MainWindow(object):
         self.label.setObjectName("label")
         self.File_link = QtWidgets.QLineEdit(self.centralwidget)
         self.File_link.setGeometry(QtCore.QRect(440, 60, 431, 21))
+        self.File_link.setText("")
         self.File_link.setObjectName("File_link")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(30, 90, 391, 21))
@@ -38,10 +39,11 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName("label_2")
         self.File_location = QtWidgets.QLineEdit(self.centralwidget)
         self.File_location.setGeometry(QtCore.QRect(440, 90, 341, 21))
+        self.File_location.setText("")
         self.File_location.setObjectName("File_location")
         self.Browse_Files = QtWidgets.QPushButton(self.centralwidget)
         self.Browse_Files.setGeometry(QtCore.QRect(780, 90, 91, 21))
-        self.Browse_Files.clicked.connect(self.browse_file)
+        self.Browse_Files.clicked.connect(self.browse_file)###################3
 
         font = QtGui.QFont()
         font.setPointSize(10)
@@ -50,94 +52,148 @@ class Ui_MainWindow(object):
         self.Browse_Files.setFont(font)
         self.Browse_Files.setObjectName("Browse_Files")
         self.Start_Download = QtWidgets.QPushButton(self.centralwidget)
-        self.Start_Download.setGeometry(QtCore.QRect(340, 120, 201, 28))
-        self.Start_Download.clicked.connect( self.start_download )
+        self.Start_Download.setGeometry(QtCore.QRect(330, 120, 201, 28))
+        self.Start_Download.clicked.connect(self.start_download)########################3
 
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
         self.Start_Download.setFont(font)
+        self.Start_Download.setAutoDefault(False)
         self.Start_Download.setObjectName("Start_Download")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(630, 520, 111, 28))
+        self.pause_all = QtWidgets.QPushButton(self.centralwidget)
+        self.pause_all.setGeometry(QtCore.QRect(630, 520, 111, 28))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(760, 520, 101, 28))
+        self.pause_all.setFont(font)
+        self.pause_all.setObjectName("pause_all")
+        self.cancel_all = QtWidgets.QPushButton(self.centralwidget)
+        self.cancel_all.setGeometry(QtCore.QRect(760, 520, 101, 28))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
-        self.pushButton_2.setFont(font)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(500, 520, 111, 28))
+        self.cancel_all.setFont(font)
+        self.cancel_all.setObjectName("cancel_all")
+        self.resume_all = QtWidgets.QPushButton(self.centralwidget)
+        self.resume_all.setGeometry(QtCore.QRect(500, 520, 111, 28))
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setWeight(75)
-        self.pushButton_3.setFont(font)
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.resume_all.setFont(font)
+        self.resume_all.setObjectName("resume_all")
         self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
         self.scrollArea.setGeometry(QtCore.QRect(9, 156, 871, 361))
+        self.scrollArea.setAcceptDrops(False)
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 869, 359))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.progressBar1 = QtWidgets.QProgressBar(self.scrollAreaWidgetContents)
+        self.progressBar1.setGeometry(QtCore.QRect(20, 40, 821, 23))
+        self.progressBar1.setProperty("value", 0)
+        self.progressBar1.setObjectName("progressBar1")
+        self.Filename1 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.Filename1.setGeometry(QtCore.QRect(20, 10, 781, 20))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.Filename1.setFont(font)
+        self.Filename1.setObjectName("Filename1")
+        self.cancel1 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.cancel1.setGeometry(QtCore.QRect(270, 70, 121, 28))
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        font.setBold(True)
+        font.setWeight(75)
+        self.cancel1.setFont(font)
+        self.cancel1.setObjectName("cancel1")
+        self.Pause1 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.Pause1.setGeometry(QtCore.QRect(20, 70, 121, 28))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.Pause1.setFont(font)
+        self.Pause1.setObjectName("Pause1")
+        self.Resume1 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.Resume1.setGeometry(QtCore.QRect(140, 70, 131, 28))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.Resume1.setFont(font)
+        self.Resume1.setObjectName("Resume1")
+        self.timeremaning1 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.timeremaning1.setGeometry(QtCore.QRect(720, 70, 141, 20))
+        self.timeremaning1.setObjectName("timeremaning1")
+        self.downloaded1 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.downloaded1.setGeometry(QtCore.QRect(480, 70, 231, 20))
+        self.downloaded1.setObjectName("downloaded1")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         MainWindow.setCentralWidget(self.centralwidget)
 
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 893, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-        # return([file_link,file_location])
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.Heading.setText(_translate("MainWindow", "File Downloader "))
         self.label.setText(_translate("MainWindow", "Enter the URL of file you want to download ->"))
-        self.File_link.setText(_translate("MainWindow", "Please enter the Link"))
+        self.File_link.setPlaceholderText(_translate("MainWindow", "Please enter the Link"))
         self.label_2.setText(_translate("MainWindow", "Enter the location of your file to be saved ->"))
-        self.File_location.setText(_translate("MainWindow", "Enter Location You want to save "))
+        self.File_location.setPlaceholderText(_translate("MainWindow", "Please enter the Location You want to save"))
         self.Browse_Files.setText(_translate("MainWindow", "Browse"))
         self.Start_Download.setText(_translate("MainWindow", "Start The Download "))
-        self.pushButton.setText(_translate("MainWindow", "Pause All"))
-        self.pushButton_2.setText(_translate("MainWindow", "Cancel All"))
-        self.pushButton_3.setText(_translate("MainWindow", "Resume All"))
+        self.pause_all.setText(_translate("MainWindow", "Pause All"))
+        self.cancel_all.setText(_translate("MainWindow", "Cancel All"))
+        self.resume_all.setText(_translate("MainWindow", "Resume All"))
+        self.Filename1.setText(_translate("MainWindow", "FILE LOCATION ->"))
+        self.cancel1.setText(_translate("MainWindow", "Cancel Download"))
+        self.Pause1.setText(_translate("MainWindow", "Pause Download"))
+        self.Resume1.setText(_translate("MainWindow", "Resume Download"))
+        self.timeremaning1.setText(_translate("MainWindow", "TIME REMAIN->"))
+        self.downloaded1.setText(_translate("MainWindow", "DOWNLOADED->"))
 
+######################################################################################################################3
     def start_download(self):
         file_link = self.File_link.text()
         file_location = self.File_location.text()
-        import downloading as dow
-        dow.download(file_link,file_location)
+        #import downloading as dow
+        self.File_location.setText('')
+        self.File_link.setText('')
+        self.Filename1.setText("FILE LOCATION ->"+file_location)
+
+        with open(file_location, 'wb') as f:
+            response = requests.get(file_link, allow_redirects=True, stream=True)
+            total_length = response.headers.get("content-length")
+            if total_length is None:
+                    f.write(response.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in response.iter_content(chunk_size=16384):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(100 * dl / total_length)
+                    self.downloaded1.setText("DOWNLOADED->"+str(dl)+"/"+str(total_length))
+                    #time.sleep(0.1)
+                    self.progressBar1.setValue(done)
+                    print(done)
+
+        #dow.download(file_link, file_location)
 
     def browse_file(self):
         self.open_dialog_box()
     def open_dialog_box(self):
         file_location = QFileDialog.getSaveFileName()
         self.File_location.setText(file_location[0])
-
-
-
+########################################################################################################################3
 if __name__ == "__main__":
     import sys
-
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
